b35e2d325783b44168d7b71376872e65c8cc6be9
Cdelgado1211 <cdelgado@danaconnect.com>
2026-03-20 13:53:54 -0400

    Identity Pool

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/global.css";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: "us-east-1:bac71bc9-7868-4d38-a967-2434283e0bf0",
+    region: "us-east-1"
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
